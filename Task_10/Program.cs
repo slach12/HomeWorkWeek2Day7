21402dc3191687618bf681e591ae7827b39f222e
slach12 <slach12@wp.pl>
2023-10-05 01:05:12 +0200

    Praca domowa z dnia 7

---
+++ Task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszy bok trójkąta:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugi bok trójkąta:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj trzeci bok trójkąta");
+            double c = double.Parse(Console.ReadLine());
+
+            bool condition1 = a + b > c;
+            bool condition2 = b + c > a;
+            bool condition3 = c + a > b;
+
+            if (condition1 && condition2 && condition3) 
+            {
+                Console.WriteLine("Można zbudować trójkąt");
+            }
+        }
+    }
+}
